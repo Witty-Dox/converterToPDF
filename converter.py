cd04c81bde3c34bf0d24cda9da432c185a95dd95
Chikirao <ivankonanch@gmail.com>
2023-02-25 11:36:10 +0300

    beta v1, firstway added.

--- converter.py
+++ converter.py
@@ -1,11 +1,17 @@
 from fpdf import FPDF
+import os
+from sys import exit
+
+clear = lambda: os.system('cls')
+
+clear()
 
 pdf = FPDF()
 pdf.add_page()
 pdf.add_font('DejaVu', '', 'fonts/DejaVuSansCondensed.ttf', uni=True)
-pdf.set_font("DejaVu", size = 15)
 
 def choose():
+    clear()
     way = input('Введите режим работы:\n1 - быстрый режим, создаёт pdf файл из вводимых в консоль строк\n2 - дополненый режим, создаёт pdf файл из текстового файла input.txt (параметры заполнения в README.)\n|')
 
     if way not in ['1', '2']:
@@ -18,23 +24,32 @@ def choose():
         firstway(outname)
 
 def firstway(outname):
-
+    clear()
     try:
         lncount = int(input('\nВведите количество заголовков: |'))
     except ValueError:
         print('\nВы ввели не число а текст!')
         firstway(outname)
     
+    if lncount == 0:
+        print('Вы ввели 0, будет создан пустой файл!')
+        firstway()
+
     for line in range(lncount):
-        print(line)
+        title = input(f'Введите заголовок №{line + 1}: |')
+        paragraph = input(f'Введите параграф №{line + 1}: |')
+        pdf.set_font("DejaVu", size = 15)
+        pdf.multi_cell(0, 30, txt = title, align = 'C')
+        pdf.set_font("DejaVu", size = 12)
+        pdf.multi_cell(0, 10, txt = f'      {paragraph}', align = 'J')
+    
+    pdf.output(f"{outname}.pdf")
+
+    print(f'Документ готов! Смотрите файл {outname}.pdf')
+
+    exit()
+
 
 choose()
 
-
 # pdf.set_text_color(79, 90, 97)
-
-# pdf.multi_cell(0, 30, txt = "GeeksforGeeks", align = 'C')
- 
-# pdf.multi_cell(0, 10, txt = "", align = 'C')
-
-# pdf.output(".pdf")
