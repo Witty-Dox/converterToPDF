3cb6e22d98071839bd1376fb484d4967624756e7
Chikirao <ivankonanch@gmail.com>
2023-02-25 12:25:57 +0300

    release v1

--- converter.py
+++ converter.py
@@ -1,11 +1,18 @@
 from fpdf import FPDF
+import os
+from sys import exit
+
+clear = lambda: os.system('cls')
+
+clear()
 
 pdf = FPDF()
 pdf.add_page()
 pdf.add_font('DejaVu', '', 'fonts/DejaVuSansCondensed.ttf', uni=True)
-pdf.set_font("DejaVu", size = 15)
+pdf.add_font('DejaVuBold', '', 'fonts/DejaVuSansCondensed-Bold.ttf', uni=True)
 
 def choose():
+    clear()
     way = input('Введите режим работы:\n1 - быстрый режим, создаёт pdf файл из вводимых в консоль строк\n2 - дополненый режим, создаёт pdf файл из текстового файла input.txt (параметры заполнения в README.)\n|')
 
     if way not in ['1', '2']:
@@ -15,50 +22,70 @@ def choose():
     outname = input('\nВведите название итогового pdf файла: |')
 
     if way == '1':
+        clear()
         firstway(outname)
     if way == '2':
-        secondway()
-    pdf.output(f'{outname}.pdf')
+        secondway(outname)
+
 def firstway(outname):
     try:
         lncount = int(input('\nВведите количество заголовков: |'))
     except ValueError:
         print('\nВы ввели не число а текст!')
-        firstway()
-    if lncount == None:
-        print('Вы ничего не ввели!')
-    for line in range(1, lncount+1):
-        title = input(f'Введите заголовок №{line}: \n')
-        #................................................................
+        firstway(outname)
     
-def secondway():
-    with open('input.txt') as input:
+    if lncount == 0:
+        print('Вы ввели 0, будет создан пустой файл!')
+        firstway()
+
+    for line in range(lncount):
+        title = input(f'Введите заголовок №{line + 1}: |')
+        paragraph = input(f'Введите параграф №{line + 1}: |')
+        pdf.set_font("DejaVuBold", size = 15)
+        pdf.multi_cell(0, 30, txt = title, align = 'C')
+        pdf.set_font("DejaVu", size = 12)
+        pdf.multi_cell(0, 10, txt = f'      {paragraph}', align = 'J')
+    
+    pdf.output(f"{outname}.pdf")
+    clear()
+    print(f'Документ готов! Смотрите файл {outname}.pdf')
+
+    exit()
+    
+def secondway(outname):
+    clear()
+    with open('input.txt', encoding='utf-8') as input:
         for line in input:
             splitted = line.split()
-            if splitted[0] == '!T':
-                red = float(splitted[1])
-                green = float(splitted[2])
-                blue = float(splitted[3])
-                pdf.set_text_color(red, green, blue)
-                joined = ' '.join(splitted[4:])
-                pdf.multi_cell(100, 10, joined, align="C")
-            elif splitted[0] == '!P':
-                red = float(splitted[1])
-                green = float(splitted[2])
-                blue = float(splitted[3])
-                pdf.set_text_color(red, green, blue)
-                joined = ' '.join(splitted[4:])
-                pdf.multi_cell(100, 10, joined, align="L")
+            if len(splitted) == 0:
+                continue
             else:
-                print("README.MD")
+                if splitted[0] == '!T':
+                    pdf.set_font("DejaVuBold", size = 15)
+                    red = float(splitted[1])
+                    green = float(splitted[2])
+                    blue = float(splitted[3])
+                    pdf.set_text_color(red, green, blue)
+                    joined = ' '.join(splitted[4:])
+                    pdf.multi_cell(0, 10, joined, align="C")
+                elif splitted[0] == '!P':
+                    pdf.set_font("DejaVu", size = 12)
+                    red = float(splitted[1])
+                    green = float(splitted[2])
+                    blue = float(splitted[3])
+                    pdf.set_text_color(red, green, blue)
+                    joined = ' '.join(splitted[4:])
+                    pdf.multi_cell(0, 10, f'        {joined}', align="L")
+                else:
+                    pdf.set_font("DejaVu", size = 12)
+                    pdf.set_text_color(0, 0, 0)
+                    joined = ' '.join(splitted[4:])
+                    pdf.multi_cell(0, 10, f'        {joined}', align="L")
+    
+    pdf.output(f"{outname}.pdf")
+    clear()
+    print(f'Документ готов! Смотрите файл {outname}.pdf')
+
+    exit()
             
 choose()
-
-
-# pdf.set_text_color(79, 90, 97)
-
-# pdf.multi_cell(0, 30, txt = "GeeksforGeeks", align = 'C')
- 
-# pdf.multi_cell(0, 10, txt = "", align = 'C')
-
-# pdf.output(".pdf")
